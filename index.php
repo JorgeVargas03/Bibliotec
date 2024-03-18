478b506ed4d549fc230633d1e9db052c3853c93a
JorgeVargas <vargas.jorge.1jtv@gmail.com>
2024-03-18 13:21:39 -0700

    Ya funciona menu desplegable

--- index.php
+++ index.php
@@ -36,6 +36,8 @@ session_start();
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
     <!--Termmina Bootstrap-->
 </head>
 <body>
@@ -55,43 +57,42 @@ session_start();
     <div class="container-fluid">
         <div class="row">
             <!-- Barra de navegación izquierda -->
-        <nav class="col-md-3 col-lg-2 d-md-block bgcol sidebar navbar navbar-expand-lg">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <nav class="col-md-3 col-lg-2 d-md-block bgcol sidebar navbar navbar-expand-md">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
-        </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
         <div class="sidebar-sticky">
             <ul class="nav flex-column nav-texto-negro">
                 <li class="nav-item active">
                     <a class="nav-link" href="#" id="colSide">Inicio</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link dropdown-toggle bg-orange" href="#" id="carrerasDropdown colSide" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Carreras</a>
-                    <div class="dropdown-menu" aria-labelledby="carrerasDropdown">
+                <li class="nav-item dropdown bg-orange">
+                    <a class="nav-link dropdown-toggle" href="#" id="carrerasDropdown colSide" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Carreras</a>
+                    <div class="dropdown-menu absolute-menu" aria-labelledby="carrerasDropdown">
                         <a class="dropdown-item" href="#">Carrera 1</a>
                         <a class="dropdown-item" href="#">Carrera 2</a>
                         <a class="dropdown-item" href="#">Carrera 3</a>
                     </div>
                 </li>
-                <li class="nav-item dropdown bg-transparent">
-                    <a class="nav-link dropdown-toggle" href="#" id="perfilDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Perfil</a>
-                    <div class="dropdown-menu" aria-labelledby="perfilDropdown">
+                <li class="nav-item dropdown bg-orange">
+                    <a class="nav-link dropdown-toggle" href="#" id="perfilDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Perfil</a>
+                    <div class="dropdown-menu absolute-menu" aria-labelledby="perfilDropdown">
                         <a class="dropdown-item" href="#">Editar Perfil</a>
                         <a class="dropdown-item" href="#">Cambiar Contraseña</a>
                     </div>
                 </li>
-                <li class="nav-item dropdown bg-transparent">
-                    <a class="nav-link dropdown-toggle" href="#" id="contactoDropdown colSide" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Contacto</a>
-                    <div class="dropdown-menu" aria-labelledby="contactoDropdown">
+                <li class="nav-item dropdown bg-orange">
+                    <a class="nav-link dropdown-toggle" href="#" id="contactoDropdown colSide" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Contacto</a>
+                    <div class="dropdown-menu absolute-menu" aria-labelledby="contactoDropdown">
                         <a class="dropdown-item" href="#">Información de Contacto</a>
                         <a class="dropdown-item" href="#">Formulario de Contacto</a>
                     </div>
                 </li>
             </ul>
-            </div>
-            </div>
-            </nav>
-
+        </div>
+    </div>
+</nav>
             <!-- Contenido principal -->
             <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
                 <div class="container mt-3">
