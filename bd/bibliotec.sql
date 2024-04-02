d905d6969dd8870425a0bf6a04d523fef34ba909
JorgeVargas <vargas.jorge.1jtv@gmail.com>
2024-04-01 21:26:36 -0700

    Base de Datos actualizada

--- bd/bibliotec.sql
+++ bd/bibliotec.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 02-04-2024 a las 02:46:59
+-- Tiempo de generación: 02-04-2024 a las 04:24:22
 -- Versión del servidor: 8.0.32
 -- Versión de PHP: 8.0.26
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `bibliotec`
 --
+CREATE DATABASE IF NOT EXISTS `bibliotec` DEFAULT CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci;
+USE `bibliotec`;
 
 -- --------------------------------------------------------
 
